c7916bbf0051332da9aaf35241fe0ee296c9b8b1
TarasSabat <tarsab25@gmail.com>
2024-06-15 10:45:00 +0300

    1

---
+++ hw_14/src/conf/messages.py
@@ -0,0 +1,5 @@
+ACCOUNT_EXIST = "Account already exists!"
+INCORRECT_EMAIL_PASSWORD = "Incorrect email or password"
+EMAIL_NOT_CONFIRMED = "Email not confirmed"
+INVALID_REFRESH_TOKEN = "Invalid refresh token"
+VERIFICATION_ERROR = "Verification error"
